3e74954b8a3a72ce5358af398d160c8f654c04bd
Daniel Rothamel <daniel.rothamel@gmail.com>
2021-09-27 09:44:12 -0400

    first files created & updated

---
+++ main.tf
@@ -0,0 +1,51 @@
+resource "aws_ecs_cluster" "cluster" {
+  name = "example-ecs-cluster"
+
+  capacity_providers = ["FARGATE_SPOT", "FARGATE"]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+  }
+
+  setting {
+    name  = "containerInsights"
+    value = "disabled"
+  }
+}
+
+module "ecs-fargate" {
+  source = "umotif-public/ecs-fargate/aws"
+  version = "~> 6.1.0"
+
+  name_prefix        = "ecs-fargate-example"
+  vpc_id             = aws_vpc.ecs_vpc
+  private_subnet_ids = [aws_subnet.private_subnet_1, aws_subnet.private_subnet_2]
+
+  cluster_id         = aws_ecs_cluster.cluster.id
+
+  task_container_image   = "centos:latest"
+  task_definition_cpu    = 256
+  task_definition_memory = 512
+
+  task_container_port             = 80
+  task_container_assign_public_ip = true
+
+  load_balanced = false
+
+  target_groups = [
+    {
+      target_group_name = "tg-fargate-example"
+      container_port    = 80
+    }
+  ]
+
+  health_check = {
+    port = "traffic-port"
+    path = "/"
+  }
+
+  tags = {
+    Environment = "test"
+    Project = "Test"
+  }
+}
