448db838e6c63bdf87ca552fd4ac24860dda60c9
Daniel Rothamel <daniel.rothamel@gmail.com>
2021-09-28 20:25:28 -0400

    changed variables and providers to remove keys

--- providers.tf
+++ providers.tf
@@ -15,6 +15,4 @@ provider "docker" {}
 
 provider "aws" {
   region = var.region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
--- variables.tf
+++ variables.tf
@@ -11,15 +11,3 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
-
-# AWS keys
-
-variable "aws_access_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "aws_secret_key" {
-  type      = string
-  sensitive = true
-}
